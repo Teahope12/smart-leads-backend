0af4640e1d0305e540872f4aa58b43c8ce6d8ded
Teahope12 <gtiyasha274@gmail.com>
2026-05-21 00:59:29 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost'],
+  origin: ['http://localhost:5173', 'https://papaya-duckanoo-8f8cc5.netlify.app/', 'https://service-hive-5cdd34.netlify.app/'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
